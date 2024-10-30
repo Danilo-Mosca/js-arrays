e2d3ff96d0a0504a51f6ff76bf061c608750d166
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-10-30 16:55:29 +0100

    fix comments

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -17,7 +17,7 @@ console.log(reversedTeachers);
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 
-// Riporto gli l'array con l'ordine originale
+// Riporto l'array con l'ordine originale
 teachers.reverse();
 // Assegno all'array longNames i valori contenuti nell'array teachers
 const longNames = teachers.slice();
