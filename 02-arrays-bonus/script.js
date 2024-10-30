c60a2f0160e9f27d807e43f45e086018f2b54dc2
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-10-30 17:01:29 +0100

    Complete bonus exercise

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -17,7 +17,8 @@ console.log(reversedTeachers);
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 
-// Riporto l'array con l'ordine originale
+// // Riporto l'array con l'ordinamento originale (con il reverse viene modificato anche 
+// l'ordine dell'array "teachers" oltre che quello di "reversedTeachers"
 teachers.reverse();
 // Assegno all'array longNames i valori contenuti nell'array teachers
 const longNames = teachers.slice();
@@ -33,6 +34,6 @@ for (let i = 0; i < longNames.length; i++) {
 }
 console.log(longNames);
 
-
-
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(1,1);
+console.log(teachers);
